cd617e0429d5ce95c141d2f25a73a6f8cd992636
zhanxw <zhanxw@gmail.com>
2020-12-31 07:33:50 -0600

    A working version

--- src/App.js
+++ src/App.js
@@ -3,33 +3,13 @@ import Header from './Header'
 import Content from './Content'
 import ScrollToTop from './ScrollToTop'
 
-// import './scroll_to_top.css';
-// import scroll_to_to from './scroll_to_top'
-
-//import 'bootstrap';
-
-// require('bootstrap')  
-// import logo from './logo.svg';
-// import './App.css';
-
-//export default 
-
-///export default 
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Header />
-//       <Content />
-//       <BackToTop />
-//     </div>
-//   );
-// }
+import background from "./background.png";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div style={{ backgroundImage: `url(${background})`}} 
+      >
         <Header />
         <Content />
         <ScrollToTop />
